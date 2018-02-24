e3b26b07262dd463aece4aa0ec1ccb278c7d0413
Emile Fokkema <emilefokkema@gmail.com>
2018-02-24 10:10:13 +0100

    initial

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var c = new Class1();
+            Assert.AreEqual(c.GetOne(), 1);
+        }
+    }
+}
