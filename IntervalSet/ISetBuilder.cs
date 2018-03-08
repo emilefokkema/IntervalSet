73d7688aecd16db56242bc5d710204749a49b71d
Emile Fokkema <emilefokkema@gmail.com>
2018-03-08 17:15:31 +0100

    docs

--- IntervalSet/ISetBuilder.cs
+++ IntervalSet/ISetBuilder.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 
 namespace IntervalSet
 {
+    /// <summary>
+    /// Builds <see cref="IIntervalSet{T}"/>s based on <typeparamref name="TInterval"/>s
+    /// </summary>
+    /// <typeparam name="TSet"></typeparam>
+    /// <typeparam name="TInterval"></typeparam>
+    /// <typeparam name="T"></typeparam>
     public interface ISetBuilder<out TSet, TInterval, T>
         where T : IEquatable<T>, IComparable<T>
     {
+        /// <summary>
+        /// Returns a <typeparamref name="TSet"/> based on a list of <typeparamref name="TInterval"/>s
+        /// </summary>
+        /// <param name="intervals"></param>
+        /// <returns></returns>
         TSet MakeSet(IList<TInterval> intervals);
+
+        /// <summary>
+        /// Returns a <typeparamref name="TInterval"/> representing the interval span of a list of <typeparamref name="TInterval"/>s
+        /// </summary>
+        /// <param name="intervals"></param>
+        /// <returns></returns>
         TInterval MakeNonEmptySet(IList<TInterval> intervals);
     }
 }
